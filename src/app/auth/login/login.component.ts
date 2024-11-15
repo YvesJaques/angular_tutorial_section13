35d680939e3dc1fda0cdc67bd500ca33eef9ee37
Yves Jaques <yves.jaques@asksuite.com>
2024-11-15 17:50:09 -0300

    Extracting User Input Values

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,8 +11,11 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class LoginComponent {
 
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email
+    const enteredPassword = formData.form.value.password
+
+    console.log(enteredEmail, enteredPassword)
   }
 
 }
