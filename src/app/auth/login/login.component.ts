70d319b21ad69fe079580e3b83197e150371fa6e
Yves Jaques <yves.jaques@asksuite.com>
2024-11-15 16:01:43 -0300

    Template-driven: Registering Form Controls

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
