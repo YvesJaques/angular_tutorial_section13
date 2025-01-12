d21141dc72ca5357f10020540eb7a3775c7dfabf
Yves Jaques <yves.jaques@asksuite.com>
2025-01-12 13:37:15 -0300

    Working with Nested Form Groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,18 +14,22 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.email, Validators.required]
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
     }),
-    confirmPassword: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
     }),
     firstName: new FormControl('', { validators: [Validators.required] }),
     lastName: new FormControl('', { validators: [Validators.required] }),
-    street: new FormControl('', { validators: [Validators.required] }),
-    number: new FormControl('', { validators: [Validators.required] }),
-    postalCode: new FormControl('', { validators: [Validators.required] }),
-    city: new FormControl('', { validators: [Validators.required] }),
     role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'
     >('student', { validators: [Validators.required] }),
     agree: new FormControl(false, { validators: [Validators.required] })
@@ -33,10 +37,6 @@ export class SignupComponent {
 
   get emailIsInvalid() {
     return this.form.controls.email.touched && this.form.controls.email.invalid && this.form.controls.email.dirty
-  }
-
-  get passwordIsInvalid() {
-    return this.form.controls.password.touched && this.form.controls.password.invalid && this.form.controls.password.dirty
   }
 
   onSubmit() {
