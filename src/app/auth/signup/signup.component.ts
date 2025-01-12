08a82ca0460056187b78e44387232f70ee3657ac
Yves Jaques <yves.jaques@asksuite.com>
2025-01-12 13:09:43 -0300

    Exercise

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  })
+
+  get emailIsInvalid() {
+    return this.form.controls.email.touched && this.form.controls.email.invalid && this.form.controls.email.dirty
+  }
+
+  get passwordIsInvalid() {
+    return this.form.controls.password.touched && this.form.controls.password.invalid && this.form.controls.password.dirty
+  }
+
+  onSubmit() {
+    console.log(this.form)
+    window.localStorage.setItem('saved-users',
+      JSON.stringify(this.form.value))
+  }
+
+  onReset() {
+    this.form.reset()
+  }
+}
